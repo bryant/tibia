602fa82872aea6e8b98d7bd200ef1d4bae3505f8
bryant <bryant@defrag.in>
2015-07-19 19:17:46 -0400

    add a bad-ass spacer

--- XXD.hs
+++ XXD.hs
@@ -12,13 +12,13 @@ xxd rowsize groupsize raw = unlines $ map (xxd_row rowsize groupsize) rows
     where rows = BStr.unpack raw `groups_of` rowsize
 
 xxd_row :: Int -> Int -> [Word8] -> String
-xxd_row rowsize grpsize row = hexes ++ spacer ++ ascii ++ "\n" ++ chars
+xxd_row rowsize grpsize row = hexes ++ spacer ++ ascii ++ '\n':chars ++ spacer
     where
     chunked = intercalate "  " . map unwords . flip groups_of grpsize
     hexes = chunked $ map as_hex row
     chars = chunked $ map ((: " ") . as_ascii ' ') row
     ascii = map (as_ascii '.') row
-    spacer = replicate (padlen + 3) ' '
+    spacer = replicate padlen ' ' ++ " │ "
     padlen = if rlen >= rowsize then 0
                                 else width_of rowsize - width_of rlen
         where rlen = length row
