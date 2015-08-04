eafcd7ae2fe600ce654ff12c19f67e7c3ab9ad8f
bryant <bryant@defrag.in>
2015-08-04 15:52:14 -0400

    convert other events to use entid

--- LibTIB/Event.hs
+++ LibTIB/Event.hs
@@ -10,9 +10,9 @@ data TibEvent
     -- ^ message, sender, possible private recipient
     | UpdateSectorEnts Node (IntMap.IntMap Entity)
     | Attacked EntID EntID Word16 Bool  -- ^ damage
-    | SetShipResources Word32 Resources
-    | EntityArrival Word32 Entity
-    | EntityDepart Word32 DepartType
+    | SetShipResources EntID Resources
+    | EntityArrival EntID Entity
+    | EntityDepart EntID DepartType
     | Unknown Word8 ByteString
     deriving Show
 
