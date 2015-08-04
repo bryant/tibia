48e9dafddb7f86dc03a33bc3dbd1258184574581
bryant <bryant@defrag.in>
2015-08-04 15:49:33 -0400

    move events (nee tib response) into own module

---
+++ LibTIB/Event.hs
@@ -0,0 +1,18 @@
+module LibTIB.Event where
+
+import Data.Serialize
+
+data TibEvent
+    = Challenge ByteString Word8  -- ^ iv and version byte
+    | Alive
+    | Chat ByteString ByteString ByteString ChatType
+    -- ^ message, sender, possible private recipient
+    | UpdateSectorEnts Node (IntMap.IntMap Entity)
+    | Attacked EntID EntID Word16 Bool  -- ^ damage
+    | SetShipResources Word32 Resources
+    | EntityArrival Word32 Entity
+    | EntityDepart Word32 DepartType
+    | Unknown Word8 ByteString
+    deriving Show
+
+newtype TibPacket t = TibEvent t deriving Show
