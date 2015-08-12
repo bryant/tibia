8421dd1b2e835069f5b1934d8b39724753941587
bryant <bryant@defrag.in>
2015-08-12 17:25:24 -0400

    request and packet parsing

--- LibTIB/Packet.hs
+++ LibTIB/Packet.hs
@@ -1,24 +1,22 @@
 module LibTIB.Packet where
 
+import qualified Data.ByteString as BStr
 import qualified Data.Serialize as Ser
-import Data.Serialize (Serialize)
-import LibTIB.Event ()  -- imports serialize instances
-import LibTIB.Request (request_code)
+import Data.Serialize (Serialize(..), encode, decode, putWord16be,
+                       putByteString, getWord16be, getByteString, Get)
 
 newtype TibPacket t = TibPacket t deriving Show
 
 instance Serialize t => Serialize (TibPacket t) where
     put (TibPacket req) = do
-        let pktdat = Ser.encode req
-        let len = BStr.length pktdat + 1  -- +1 for code
-        Ser.putWord16be len
-        Ser.putWord8 $ request_code req
-        Ser.putByteString pktdat
+        let pktdat = encode req
+        putWord16be . fromIntegral $ BStr.length pktdat
+        putByteString pktdat
 
     get = do
-        len <- Ser.getWord16be  -- packet length sans first two
-        g <- Ser.getByteString $ fromIntegral len
+        len <- getWord16be  -- packet length sans first two
+        g <- getByteString $ fromIntegral len
         -- ^ slurp entire packet length to ensure parsing resumes on a packet
         -- boundary, even upon failure. also, leave in the command code byte
         -- for event deserializer to dispatch on.
-        either fail return $ Ser.decode g
+        either fail (return . TibPacket) $ decode g
