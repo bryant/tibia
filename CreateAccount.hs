48dda3442ea769746eb46633b9dea334be3e8c48
bryant <bryant@defrag.in>
2015-08-13 00:42:54 -0400

    update account creation tool to new api

--- CreateAccount.hs
+++ CreateAccount.hs
@@ -1,6 +1,6 @@
-module CreateAccount where
+{-# LANGUAGE OverloadedStrings #-}
 
-import Pwned hiding (main)
+module CreateAccount where
 
 import Network.Socks5
     ( socksConnectName
@@ -10,7 +10,7 @@ import Network.Socks5
     , defaultSocksConf
     , defaultSocksConfFromSockAddr
     )
-import Data.Serialize (runGet)
+import Data.Serialize (decode, encode)
 import Network.Socket
     ( withSocketsDo
     , socket
@@ -22,23 +22,38 @@ import Network.Socket
 import Network.Socket.ByteString (recv, send)
 import Network.BSD (getProtocolNumber, getHostByName, hostAddress)
 import Control.Applicative ((<$>))
+import Control.Monad (forever)
+import LibTIB.Event (TibEvent(Challenge), decode_event)
+import LibTIB.Request (TibRequest(NewAcc, Auth), encode_request)
+import LibTIB.Common (Account(..), server_ip, Server(ServGray))
+import Util.XXD (xxd)
 
 tor = SockAddrInet 9050 . hostAddress <$> getHostByName "127.0.0.1"
 
 tor_connect sock host port = tor >>= \t -> socksConnectName sock t host port
 
+doctek = Account "i\x9814you" "i\x9814you" "8ef4d93d0123316a4fbc85f65446b156"
+            "android os mifitech44u" "client_v1.7-gp"
+
 main = withSocketsDo $ do
     sock <- socket AF_INET Stream =<< getProtocolNumber "tcp"
-    tor_connect sock tib 32040
+    tor_connect sock server_ip 32040
     shouldbeiv <- recv sock 1024
-    case runGet get_tib_response shouldbeiv of
+    case decode_event shouldbeiv of
         Left wut -> error wut
         Right (Challenge iv ver) -> do
             putStrLn $ "Server version: " ++ show ver
             putStrLn $ "IV received: " ++ show iv
             putStrLn $ "Creating new account: " ++ show doctek
-            send sock . put_tib_request $ NewAcc iv doctek
-            return ()
+            send sock . encode_request $ NewAcc iv doctek ServGray
+            recvdump sock 65536
+            send sock . encode_request $ Auth iv doctek ServGray
+            forever $ recvdump sock 1024
 
         Right unknown-> do
             putStrLn $ "Unknown response from server: " ++ show unknown
+
+recvdump sock n = do
+    stuff <- recv sock n
+    putStrLn $ "got stuff!\n\n"
+    putStrLn $ xxd 16 4 stuff
