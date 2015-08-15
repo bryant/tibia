d8ce27464c57fd1f551f785895e57e75ffe765a1
bryant <bryant@defrag.in>
2015-08-15 05:29:36 -0400

    add newnym tor helper call

--- src/Util/Sock.hs
+++ src/Util/Sock.hs
@@ -2,9 +2,13 @@ module Util.Sock where
 
 import Network.Socket
     ( socket
+    , close
     , Family(AF_INET)
     , SocketType(Stream)
     , Socket
+    , connect
+    , send
+    , SockAddr(SockAddrInet)
     )
 import Network.Socks5
     ( socksConnect
@@ -13,7 +17,7 @@ import Network.Socks5
     , SocksConf
     , defaultSocksConf
     )
-import Network.BSD (getProtocolNumber)
+import Network.BSD (getProtocolNumber, getHostByName, hostAddress)
 import Data.Word (Word16)
 import Data.ByteString (ByteString)
 
@@ -30,3 +34,14 @@ sconnect :: SocksConf -> ByteString -> Word16 -> IO Socket
 sconnect socksiface dhost dport = fst `fmap` socksConnect socksiface socksaddr
     where
     socksaddr = SocksAddress (SocksAddrDomainName dhost) $ fromIntegral dport
+
+-- | requests tor daemon for new exit ip. hostname/port are those of control
+-- iface, typically localhost:9051
+newnym :: String -> Word16 -> IO ()
+newnym hostname port = do
+    sock <- tcp
+    addr <- hostAddress `fmap` getHostByName hostname
+    connect sock $ SockAddrInet (fromIntegral port) addr
+    send sock "AUTHENTICATE\n"
+    send sock "SIGNAL NEWNYM\n"
+    close sock
